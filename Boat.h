d0c10d0d78e365fe51c2b53e6a48204ef711e29e
Amanda <amandaa.nicole.lee@gmail.com>
2019-10-04 13:36:50 -0400

    Implementation of Boat.h (my vehicle)

---
+++ Boat.h
@@ -0,0 +1,24 @@
+//
+// Created by Amanda Lee
+//
+
+#ifndef DRIVINGSIMULATOR_BOAT_H
+#define DRIVINGSIMULATOR_BOAT_H
+
+#include "PoweredVehicle.h" 
+
+class Boat : public PoweredVehicle {
+
+private:
+	int numberOfMotors;
+public:
+	Boat();
+	explicit Boat(string brand, string model, string fuelType, int numberOfMotors);
+	virtual ~Boat(); 
+	int getNumberOfMotors();
+	void setNumberOfMotors();
+	virtual double mileageEstimate(double time);
+	virtual string toString();
+
+};
+#endif //DRIVINGSIMULATOR_BOAT_H 
